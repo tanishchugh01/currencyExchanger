e8208fad251227426d21c341eb2f0adfc483bd23
tanishchugh01 <tanishchugh01@gmail.com>
2021-12-02 01:16:15 +0530

    javascript fetch changed

--- assets/script.js
+++ assets/script.js
@@ -2,46 +2,49 @@ const apiKey="03173eb866cc2df4df13";
 
 async function symbolsPlease() {
     const response = await fetch(
-        "https://free.currconv.com/api/v7/currencies?apiKey="+apiKey
-    );
-
+        "https://free.currconv.com/api/v7/currencies?apiKey=03173eb866cc2df4df13"
+    , {mode: 'no-cors'}
+        
+);
     return await response.json();
 }
 
 async function ratesPlease() {
     const response = await fetch(
-        "https://free.currconv.com/api/v7/apiKey="+apiKey
+        "https://free.currconv.com/api/v7/currencies?apiKey="+apiKey
+        , {mode: 'no-cors'}
     );
 
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
     return await response.json();
 }
 
-symbolsPlease().then(
+// symbolsPlease().then(
     
-    function (data) {
+//     function (data) {
         
-        const symbols = data.symbols;
+//         const symbols = data.symbols;
 
-        const currencies = document.getElementById("currencies");
+//         const currencies = document.getElementById("currencies");
 
-        let ind = 0;
+//         let ind = 0;
 
-        for (let code in symbols) {
-            let option = document.createElement("option");
-            option.value = code;
-            option.text = symbols[code];
+//         for (let code in symbols) {
+//             let option = document.createElement("option");
+//             option.value = code;
+//             option.text = symbols[code];
 
-            currencies.appendChild(option);
+//             currencies.appendChild(option);
 
-            ind++;
-        }
-    },
+//             ind++;
+//         }
+//     },
     
-    function (error) {
-        console.log(error);
-    }
-);
+//     function (error) {
+//         console.log(error);
+//     }
+// );
 
 ratesPlease().then((data) => {console.log(data)}, (error) => {console.log(error)});
-// let symbols;
-// let select;
