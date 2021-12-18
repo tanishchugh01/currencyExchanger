4baaffcc0889598ae9f2db8cc34d608eaf908726
tanishchugh01 <tanishchugh01@gmail.com>
2021-12-18 13:21:48 +0530

    added event listeners to buttons

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-const apiKey = "03173eb866cc2df4df13";
+//api fetches
+// const apiKey = "03173eb866cc2df4df13";
 
 async function symbolsPlease() {
     const response = await fetch(
@@ -15,6 +16,7 @@ async function ratesPlease(from, to) {
     return await response.json();
 }
 
+//adding symobls to the dropdown
 symbolsPlease().then(
     function (data) {
         const symbols = data.results;
@@ -31,7 +33,7 @@ symbolsPlease().then(
 
             currencies.appendChild(option);
         }
-        
+
         console.log("Added elements");
     },
 
@@ -44,6 +46,7 @@ symbolsPlease().then(
 // ratesPlease("USD","INR").then((data) => console.log(data), (error) => {console.log(error)});
 // symbolsPlease().then((data) => {console.log(data);symbols=data}, (error) => {console.log(error)});
 
+//converting currencies
 function submit() {
     const amount = document.getElementById("amount").value;
 
@@ -68,16 +71,53 @@ function convert(amount, from, to) {
     );
 }
 
-function swapSymbols()
-{
+//swapping currencies
+function swapSymbols() {
     let to = document.getElementById("fromCurrency");
     let from = document.getElementById("toCurrency");
-    
+
     //swap
     let temp = to.value;
-    to.value=from.value;
-    from.value=temp;
+    to.value = from.value;
+    from.value = temp;
 }
 
+//calculators buttons
+let buttons = document.querySelectorAll(".btn");
+
+for (let ind in buttons) {
+    if (ind > 1) {
+        buttons[ind].addEventListener("click", (e) => {
+            if(ind==11)
+            {
+                addDecimal();
+            }
+            else if(ind==13)
+            {
+                addBackspace();
+            }
+            else
+            {
+                addNumber(e.target.innerHTML);
+            }
+            // console.log(e.target.innerHTML);
+            // console.log(ind);
+        });
+    }
+}
+
+function addNumber(num) { 
+    console.log(num+"added");
+}
+
+function addDecimal() {
+    console.log(".");
+}
+
+function addBackspace() {
+    console.log("backspace");
+}
+//13 is backspace
+//11 is .
 // print meaning of life
 // console.log(42);
